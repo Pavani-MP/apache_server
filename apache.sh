91f975dba4d21910bfcde03c78434c0d68e8c23d
Pavani-MP <52396656+Pavani-MP@users.noreply.github.com>
2019-09-05 20:56:28 -0500

    update s3 bucket name

--- apache.sh
+++ apache.sh
@@ -6,5 +6,5 @@ echo "<html><h1>Hello poli This is my webpage</h1></html>" > index.html
 chmod 777 index.html
 service httpd start
 chkconfig httpd on
-aws s3 mb s3://Muniamma
-aws s3 cp index.html s3://Muniamma
+aws s3 mb s3://muniamma
+aws s3 cp index.html s3://muniamma
